708e295e83e8a565b112c15213adc11e25b052d3
adithyasankars <adithyasankars7@gmail.com>
2024-06-13 20:49:36 +0530

    JS started  - api

---
+++ Quote Generator App/main.js
@@ -0,0 +1,7 @@
+const api_url = "https://api.quotable.io/random";
+
+async function getQuote(url){
+    const response = await fetch(url);
+    var data = await response.json();
+    console.log(data);
+}
